a247edad474844b74d689e441ec9c1ba26315d90
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-12 14:34:24 +0100

    Update sparse.dart

--- lib/sparse.dart
+++ lib/sparse.dart
@@ -2,6 +2,11 @@
 /// (c) 2023 by Andreas Schwenk <mailto:contact@compiler-construction.com>
 /// License: GPL-3.0-or-later
 
+// TODO: code doc
+
 library sparse;
 
 export 'src/sparse_base.dart';
+export 'src/generator.dart';
+export 'src/grammar.dart';
+export 'src/parser.dart';
